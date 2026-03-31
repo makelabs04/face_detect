23cebe052685944ae4ad7515b748023ece1e71b7
makelabs04 <makelabsmail@gmail.com>
2026-03-31 14:58:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1430,7 +1430,7 @@ app.get('/admin', (_, res) => {
           <div class="form-group">
             <label>Month</label>
             <select class="form-control" id="expMonth">
-              ${['January','February','March','April','May','June','July','August','September','October','November','December'].map((m,i)=>`<option value="${i+1}">${m}</option>`).join('')}
+              <option value="1">January</option><option value="2">February</option><option value="3">March</option><option value="4">April</option><option value="5">May</option><option value="6">June</option><option value="7">July</option><option value="8">August</option><option value="9">September</option><option value="10">October</option><option value="11">November</option><option value="12">December</option>
             </select>
           </div>
           <div class="form-group">
